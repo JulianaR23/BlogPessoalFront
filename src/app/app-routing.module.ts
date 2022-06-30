85df96dba81a7da3aab25157ed06498b56fe9535
Juliana <julianarayane23@hotmail.com>
2022-06-30 09:57:17 -0300

    Adicionando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+
+  {path: 'cadastrar', component: CadastrarComponent},
+  {path: 'login', component: LoginComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
