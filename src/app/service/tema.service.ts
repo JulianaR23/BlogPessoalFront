8f83b48152963978f087bae0e753d50d1277826a
Juliana <julianarayane23@hotmail.com>
2022-07-08 11:36:35 -0300

    CRUD completo de Tema

---
+++ src/app/service/tema.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TemaService {
+  
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
+
+  getAllTema(): Observable<Tema[]>{
+    return this.http.get<Tema[]>('https://blogp2022.herokuapp.com/temas', this.token)
+  }
+
+  getByIdTema(id:number): Observable<Tema>{
+    return this.http.get<Tema>(`https://blogp2022.herokuapp.com/temas/${id}`, this.token)
+
+  }
+
+  postTema(tema: Tema): Observable<Tema>{
+    return this.http.post<Tema>('https://blogp2022.herokuapp.com/temas', tema, this.token)
+  }
+
+  putTema(tema: Tema): Observable<Tema>{
+    return this.http.put<Tema>('https://blogp2022.herokuapp.com/temas', tema, this.token)
+  }
+
+  deleteTema(id:number){
+    return this.http.delete(`https://blogp2022.herokuapp.com/temas/${id}`, this.token)
+  }
+}
