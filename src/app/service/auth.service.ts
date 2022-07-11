e1cda277bf4b592a162556082b9541a79e88db64
Juliana <julianarayane23@hotmail.com>
2022-07-11 15:07:10 -0300

    novas atualizações

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
@@ -21,6 +21,11 @@ export class AuthService {
   cadastrar(usuario: Usuario): Observable<Usuario>{
     return this.http.post<Usuario>('https://blogp2022.herokuapp.com/usuarios/cadastrar', usuario)
     }
+
+  getByIdUser(id: number): Observable<Usuario>{
+    return this.http.get<Usuario>(`https://blogp2022.herokuapp.com/usuarios/${id}`)
+  }
+
 
   logado(){
     let ok: boolean = false
